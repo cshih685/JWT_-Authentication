9c5c6f40e7922f71c82c72a67dfbb988e31e12d9
cshih685 <32198222+leehon3322@users.noreply.github.com>
2020-06-16 14:50:55 -0400

    add new post for getting accessToken

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
@@ -24,7 +26,9 @@ app.post('/login', (req, res) => {
 
   const username = req.body.username
   const user = { name: username}
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  
+  const accessToken = jwt.sign(user, 
+    process.env.ACCESS_TOKEN_SECRET)
   res.json({ accessToken: accessToken })
 })
 app.listen(3000)
