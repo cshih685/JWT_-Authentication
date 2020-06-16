6f802b1fadf9d2b9e4399db46b2e5e0491b2e1e3
cshih685 <32198222+leehon3322@users.noreply.github.com>
2020-06-16 14:25:24 -0400

    add jwt, also sign and create tokens by using node require(crypto).randomBytes(64).toString(hex)

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
 const posts = [
   {
     username: 'Kyle',
@@ -15,4 +19,12 @@ app.get('/posts', (req, res) =>{
   res.json(posts)
 })
 
+app.post('/login', (req, res) => {
+  //Authenticate User
+
+  const username = req.body.username
+  const user = { name: username}
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({ accessToken: accessToken })
+})
 app.listen(3000)
